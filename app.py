80ac55ed3d88696413d51079f887e80b1385ef44
tomellericcardo <tomelleri.riccardo@gmail.com>
2018-09-10 16:38:27 +0200

    add projects list

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
+# INVIO DI FILES
+
+# Homepage
 @app.route('/')
+@app.route('/home')
 def home():
-    return 'Hello world!'
+    return send_from_directory('public/html/', 'home.html')
+
+# Altri files
+@app.route('/<cartella>/<nome_file>')
+def invia_file(cartella, nome_file):
+    return send_from_directory('public/' + cartella + '/', nome_file)
+
+
+# AVVIO DEL SERVER
 
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded = True)
